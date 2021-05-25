7441c308ad280b4f5155ffe52cc0970bad6a60ac
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-05-25 18:09:01 +0100

    fixed url bug again

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -15,7 +15,8 @@ export default class BlogList extends Component {
 
   fetchPosts = async () => {
     try {
-    d
+    const apiUrl = process.env.REACT_APP_API_URL
+    const resp = await fetch(`${apiUrl}/blogposts`)
     const data = await resp.json()
     console.log(resp)
 
