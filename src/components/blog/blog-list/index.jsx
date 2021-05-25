40a141f8dae8b1e95d5946318384592b30e1e710
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-05-25 15:22:49 +0100

    vercel changes

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -10,13 +10,18 @@ export default class BlogList extends Component {
   componentDidMount = async () => {
     const apiUrl = process.env.REACT_APP_API_URL 
     await this.fetchPosts(apiUrl)
+    
   }
 
   fetchPosts = async (apiUrl) => {
+    try {
     const resp = await fetch(`${apiUrl}/blogposts`)
     const data = await resp.json()
 
     this.setState({posts: data})
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   render() {
